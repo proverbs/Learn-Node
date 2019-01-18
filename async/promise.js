6b38dd1d2d51f6bf2a13f5c9056c760bf97f1b93
Hao Xu <379548839@qq.com>
2019-01-18 17:08:26 -0500

    added promise

--- async/promise.js
+++ async/promise.js
@@ -1,29 +1,54 @@
-// ref: https://nya.io/Node-js/promise-in-nodejs-get-rid-of-callback-hell/
+// ref: https://juejin.im/entry/56c46015c24aa800528da4d5
 
-var fs = require('fs');
-var Q = require('q');
-var deferred = Q.defer();
+var Promise = require('promise');
 
-function heavy() {
+var promise = new Promise(function(resolve, reject) {
     var count = 0;
     for (var i = 0; i < 100000; i ++) {
         for (var j = 0; j < 10000; j ++) {
             count += 1;
         }
     }
-    deferred.resolve(count); // return value
-    // deferred.reject('reject');
-    return deferred.promise;
-}
+    resolve(count);
+    // reject(-count);
+});
 
-function print() {
-    console.log(count);
-}
+promise.then(function (value) {
+    console.log('Resolved');
+    console.log(value);
+}, function (value) {
+    console.log('Rejected');
+    console.log(value);
+});
 
 
-heavy().then(function(success) {
-    console.log('success');
-    console.log(success);
-}, function(err) {
-    console.log(err);
+
+var promise1 = new Promise(function(resolve, reject) {
+    var count = 0;
+    for (var i = 0; i < 10000; i ++) {
+        for (var j = 0; j < 1000; j ++) {
+            count += 1;
+        }
+    }
+    resolve(count);
+    // reject(-count);
+});
+
+var promise2 = new Promise(function(resolve, reject) {
+    var count = 0;
+    for (var i = 0; i < 10000; i ++) {
+        for (var j = 0; j < 1000; j ++) {
+            count += 1;
+        }
+    }
+    // resolve(count);
+    reject(-count);
+});
+
+Promise.all([promise1, promise2]).then(function ([cnt1, cnt2]) {
+    console.log('Resolved');
+    console.log(cnt1 + cnt2);
+}, function (cnt) { // go into reject when there is ONE rejection, so the argument should not be an array
+    console.log('Rejected');
+    console.log(cnt);
 });
---
+++ async/promise_q.js
@@ -0,0 +1,24 @@
+// ref: https://nya.io/Node-js/promise-in-nodejs-get-rid-of-callback-hell/
+
+var fs = require('fs');
+var Q = require('q');
+var deferred = Q.defer();
+
+function heavy() {
+    var count = 0;
+    for (var i = 0; i < 100000; i ++) {
+        for (var j = 0; j < 10000; j ++) {
+            count += 1;
+        }
+    }
+    deferred.resolve(count); // return value
+    // deferred.reject('reject');
+    return deferred.promise;
+}
+
+heavy().then(function(success) {
+    console.log('success');
+    console.log(success);
+}, function(err) {
+    console.log(err);
+});
